6cf4849112c80f747cbe91374afc6ba07684ae63
Michael Hankin <me@michaelhankin.com>
2020-05-29 17:17:32 -0500

    improve game management

--- connect-four-client/src/App.js
+++ connect-four-client/src/App.js
@@ -9,22 +9,49 @@ function App() {
   const [color, setColor] = useState();
   const [winner, setWinner] = useState();
   const [playerId, setPlayerId] = useState();
+  const [sessionId, setSessionId] = useState();
+  const [joinSessionId, setJoinSessionId] = useState("");
+  const [myTurn, setMyTurn] = useState();
+  // const [waitingForOtherPlayer, setWaitingForOtherPlayer] = useState()
 
   // close ws on unmount
   useEffect(() => {
     ws.addEventListener("message", (message) => {
       const { type, ...rest } = JSON.parse(message.data);
 
+      // TODO: create reducer, map message types to actions
       switch (type) {
-        case "new":
-          setColor(rest.color);
+        case "create-session": {
+          setSessionId(rest.sessionId);
+          console.log("session id", rest.sessionId);
           setPlayerId(rest.playerId);
+          setColor(rest.color);
+          // console.log(rest.board[0]);
           setBoard(rest.board);
+          setMyTurn(true);
+          setJoinSessionId("");
           break;
-        case "reset":
+        }
+        case "join-session": {
+          console.log(message.data);
+          setPlayerId(rest.playerId);
+          setColor(rest.color);
           setBoard(rest.board);
+          setMyTurn(false);
+          setSessionId(joinSessionId);
+          setJoinSessionId("");
           break;
+        }
+        // case "new":
+        //   setColor(rest.color);
+        //   setPlayerId(rest.playerId);
+        //   setBoard(rest.board);
+        //   break;
+        // case "reset":
+        //   setBoard(rest.board);
+        //   break;
         case "move":
+          setMyTurn(true);
           if (rest.winner) {
             setWinner(rest.color);
           }
@@ -38,39 +65,88 @@ function App() {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [joinSessionId]);
 
   return (
     <div className="App">
-      {playerId && <div>Player {color}</div>}
-      {board &&
-        board.map((row, rowIndex) => (
-          <div key={rowIndex}>
-            {row.map((cell, colIndex) => (
-              <span
-                key={colIndex}
-                onClick={() => {
-                  if (!winner) {
-                    ws.send(
-                      JSON.stringify({ type: "move", colIdx: colIndex, color }),
-                    );
-                  }
-                }}
-              >
-                {cell}
-              </span>
+      {!sessionId && <div>
+        <button
+          onClick={() => {
+            const message = {
+              type: "create-session",
+            };
+            ws.send(JSON.stringify(message));
+          }}
+        >
+          create session
+        </button>
+        <button
+          onClick={() => {
+            const message = {
+              type: "join-session",
+              sessionId: joinSessionId,
+            };
+            ws.send(JSON.stringify(message));
+          }}
+        >
+          join session
+        </button>
+        <input
+          type="text"
+          value={joinSessionId}
+          onChange={(e) => {
+            setJoinSessionId(e.target.value);
+          }}
+        />
+      </div>}
+      {sessionId &&
+        <>
+          {playerId && <div>Player {color}</div>}
+          {board &&
+            board.map((row, rowIndex) => (
+              <div key={rowIndex}>
+                {row.map((cell, colIndex) => (
+                  <span
+                    key={colIndex}
+                    onClick={() => {
+                      if (!winner && myTurn) {
+                        setBoard((prevBoard) => {
+                          let moveRowIdx = -1;
+                          for (let i = prevBoard.length - 1; i >= 0; i--) {
+                            if (prevBoard[i][colIndex] === 0) {
+                              moveRowIdx = i;
+                              break;
+                            }
+                          }
+                          if (moveRowIdx !== -1) {
+                            prevBoard[moveRowIdx][colIndex] = color;
+                            ws.send(
+                              JSON.stringify(
+                                { type: "move", colIdx: colIndex, color },
+                              ),
+                            );
+                            setMyTurn(false);
+                          }
+                          return prevBoard;
+                        });
+                      }
+                    }}
+                  >
+                    {cell}
+                  </span>
+                ))}
+              </div>
             ))}
-          </div>
-        ))}
-      <button
-        onClick={() => {
-          setWinner(undefined);
-          ws.send(JSON.stringify({ type: "reset" }));
-        }}
-      >
-        reset board
-      </button>
-      {winner && <div>player {winner} wins!</div>}
+          <button
+            onClick={() => {
+              setWinner(undefined);
+              ws.send(JSON.stringify({ type: "reset" }));
+            }}
+          >
+            reset board
+          </button>
+          {winner && <div>player {winner} wins!</div>}
+        </>}
     </div>
   );
 }
--- connect-four-server/index.js
+++ connect-four-server/index.js
@@ -5,62 +5,91 @@ const Board = require("./Board");
 
 const wss = new WebSocket.Server({ port: 8080 });
 let sessionId;
-let board;
+// let board;
 let playerOneId;
 let playerTwoId;
 
+/** 
+ * session schema
+ * players: { id: Color }
+ * board: Board
+ * id: string
+ */
+const sessions = {};
+
 console.log("server listening on port 8080");
 
+// after connecting, user can either create a new game
+// or join an existing one
 wss.on("connection", (ws) => {
   console.log("new connection opened");
-  let playerId;
-  let playerColor;
-  if (!sessionId) {
-    sessionId = uuidv4();
-    console.log("new session created with id:", sessionId);
-    board = new Board();
-  }
-  if (!playerOneId) {
-    playerOneId = uuidv4();
-    console.log("player one connected. id:", playerOneId);
-    playerColor = 1;
-    playerId = playerOneId;
-  } else if (!playerTwoId) {
-    playerTwoId = uuidv4();
-    console.log("player two connected. id:", playerTwoId);
-    playerColor = 2;
-    playerId = playerTwoId;
-  }
-
-  ws.send(
-    JSON.stringify(
-      { type: "new", board: board.getState(), playerId, color: playerColor },
-    ),
-  );
 
   ws.on("message", (message) => {
     console.log("incoming message:", message);
     const { type, ...rest } = JSON.parse(message);
-    let response;
+    // let response;
 
     switch (type) {
-      case "reset":
-        board.reset();
-        response = { type, board: board.getState() };
+      case "create-session": {
+        const playerId = uuidv4();
+        const sessionId = uuidv4();
+        const color = 1;
+        const board = new Board();
+        const session = {
+          id: sessionId,
+          players: {
+            [playerId]: color,
+          },
+          board,
+        };
+        sessions[sessionId] = session;
+        const response = {
+          type,
+          board: board.getState(),
+          sessionId,
+          playerId,
+          color,
+        };
+        ws.send(JSON.stringify(response));
         break;
-      case "move":
-        const { colIdx, color } = rest;
+      }
+      case "join-session": {
+        // TODO: fail if there are already 2 players in the session
+        const playerId = uuidv4();
+        const color = 2;
+        const { board, players } = sessions[rest.sessionId];
+        players[playerId] = color;
+        const response = {
+          type,
+          board: board.getState(),
+          color,
+          playerId,
+        };
+        ws.send(JSON.stringify(response));
+        break;
+      }
+      // case "reset":
+      //   board.reset();
+      //   response = { type, board: board.getState() };
+      //   break;
+      case "move": {
+        // TODO: rename `colIdx` to `column`
+        const { colIdx, sessionId, playerId } = rest;
+        const { board, players } = sessions[sessionId];
+        const { color } = players[playerId];
         const winner = board.move(colIdx, color);
         response = { type, board: board.getState(), winner, color };
+        // TODO: send a separate message if there's a winner
+
+        for (const client of wss.clients) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(response));
+          }
+        }
         break;
+      }
       default:
         throw new Error("Invalid message type");
-    }
-
-    for (const client of wss.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(response));
-      }
     }
   });
 });
